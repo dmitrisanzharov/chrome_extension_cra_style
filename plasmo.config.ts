b97fde24396498ff61786e6fb70fda4f8fd53c41
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-08 13:21:24 +0100

    all is working

---
+++ plasmo.config.ts
@@ -0,0 +1,10 @@
+import { definePlasmoConfig } from "plasmo"
+
+export default definePlasmoConfig({
+  manifest: {
+    name: "aaa_Dmitri React Ext",
+    displayName: "aaa_Dmitri React Ext",
+    version: "0.0.1",
+    description: "aaa_dmitri_ext"
+  }
+})
